3abfc5b61bbabea7ae71c31e54e44de6c8175369
frvasileva <fanka@klearlending.com>
2019-12-10 18:01:45 +0200

    Home page improvements

--- BookApp-SPA/src/app/ui-core/home/home.component.ts
+++ BookApp-SPA/src/app/ui-core/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "src/app/_services/auth.service";
 import { SeoHelperService } from "src/app/_shared/seo-helper.service";
+import { BookService } from "src/app/_services/book.service";
 
 @Component({
   selector: "app-home",
@@ -9,14 +10,20 @@ import { SeoHelperService } from "src/app/_shared/seo-helper.service";
 })
 export class HomeComponent implements OnInit {
   isAuthenticated: boolean;
+  bookList: any;
 
   constructor(
     private authService: AuthService,
-    private seoService: SeoHelperService
+    private seoService: SeoHelperService,
+    private bookService: BookService
   ) {}
 
   ngOnInit() {
     this.isAuthenticated = this.authService.isAuthenticated();
     this.seoService.setSeoMetaTags("📖");
+
+    this.bookService.RecommendByRelevance(0).subscribe(data => {
+      this.bookList = data.items.splice(0, 6);
+    });
   }
 }
