7c36ca83c2aeed0dbefa91df55afa3a0773f87bd
frvasileva <fanka@klearlending.com>
2019-12-28 21:17:30 +0200

    routing fixes

--- BookApp.API/Models/Book.cs
+++ BookApp.API/Models/Book.cs
@@ -11,7 +11,6 @@ namespace BookApp.API.Models {
 
     public int Id { get; set; }
     public int ExternalId { get; set; }
-    public string ISBN { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string PhotoPath { get; set; }
