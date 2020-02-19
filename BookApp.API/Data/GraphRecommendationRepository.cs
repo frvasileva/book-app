f683686faa072886709d161d25ff54d643d57a67
frvasileva <fanka@klearlending.com>
2020-02-19 16:15:44 +0200

    RecommendByBooksAddedCategories algo changed

--- BookApp.API/Data/GraphRecommendationRepository.cs
+++ BookApp.API/Data/GraphRecommendationRepository.cs
@@ -35,7 +35,7 @@ namespace BookApp.API.Data {
         .Where (whereClause)
         .Return ((catalog, book, count, rand) => new {
           catalogs = Return.As<IEnumerable<BookCatalogListDto>> ("collect({catalogId:catalog.id, name:catalog.name, friendlyUrl:catalog.friendlyUrl})"),
-          bk = book.As<BookDetailsDto> ()
+            bk = book.As<BookDetailsDto> ()
         })
         .OrderByDescending ("rand()")
         .Skip (skipResults)
@@ -73,23 +73,22 @@ namespace BookApp.API.Data {
       var skipResults = currentPage * SHOW_MAX_RESULTS_PER_PAGE;
 
       var result = _graphClient.Cypher
-        .Match ("(currentUser:Profile)-[:FOLLOW_USER]->(p2:Profile)")
-        .Where (whereClause)
-        .With (@"collect(distinct p2.id) as followersIds 
-                 match (b:Book)-[r:BOOK_ADDED_TO_CATALOG]->(c:Catalog) 
-                 where r.userId  in followersIds with  collect(b) as BookTitles")
-        .Unwind ("BookTitles", "Books")
-        .Return ((Books) => new {
-          Count = Return.As<int> ("count (*)"),
-            Books = Return.As<BookDetailsDto> ("Books")
-        })
-        .OrderByDescending ("Count")
-        .Skip (skipResults).Limit (SHOW_MAX_RESULTS_PER_PAGE);
+        .Match ("(p1:Book)-[r:BOOK_ADDED_TO_CATALOG{userId:" + userId + "}]->(catalog1:Catalog)")
+        .With ("p1, collect(id(catalog1)) AS p1Catalog")
+        .Match ("(p2:Book)-[:BOOK_ADDED_TO_CATALOG]->(catalog2:Catalog)")
+        .Where ("p1 <> p2")
+        .With ("p1, p1Catalog, p2, collect(id(catalog2)) AS p2Catalog")
+        .Return ((p1, p2, similiarity) => new {
+          book = p1.As<BookDetailsDto> (),
+            recommendation = p2.As<BookDetailsDto> (),
+            similarity = Return.As<double> ("algo.similarity.jaccard(p1Catalog, p2Catalog)")
+        }).OrderByDescending ("similarity")
+        .Limit (SHOW_MAX_RESULTS_PER_PAGE);
 
       var bookList = new List<BookDetailsDto> ();
 
       foreach (var b in result.Results) {
-        var bd = b.Books;
+        var bd = b.book;
         bd.RecommendationCategory = "RELEVANCE";
 
         bookList.Add (bd);
